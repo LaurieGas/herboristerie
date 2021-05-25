3ca310a01da12ffee0a712366630c0e854f8e27e
LaurieGas <lauriegaspard@hotmail.fr>
2021-05-25 12:05:06 +0200

    commit-003
    partie connexion :
    security yaml
    encodage password
    authentification (controller et vue)

---
+++ site-vitrine/src/Controller/CompteController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+// use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class CompteController extends AbstractController
+{
+    /**
+     * @Route("/connexion", name="compte_connexion", methods={"GET","POST"})
+     */
+    public function connexion(): Response
+    {
+        // attraper l'erreur si il y en a une
+        // $error = $authenticationUtils->getLastAuthenticationError();
+
+        // dernier mail entré par l'utilisateur
+        // $lastEmail = $authenticationUtils->getLastUsername();
+
+
+        return $this->render('compte/connexion.html.twig', [
+            // 'last_email' => $lastEmail,
+            // 'error' => $error,
+        ]);
+    }
+}
---
+++ site-vitrine/src/Entity/Utilisateurs.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UtilisateursRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=UtilisateursRepository::class)
+ */
+class Utilisateurs implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $email_utilisateur;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom_utilisateur;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenom_utilisateur;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmailUtilisateur(): ?string
+    {
+        return $this->email_utilisateur;
+    }
+
+    public function setEmailUtilisateur(string $email_utilisateur): self
+    {
+        $this->email_utilisateur = $email_utilisateur;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email_utilisateur;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getNomUtilisateur(): ?string
+    {
+        return $this->nom_utilisateur;
+    }
+
+    public function setNomUtilisateur(string $nom_utilisateur): self
+    {
+        $this->nom_utilisateur = $nom_utilisateur;
+
+        return $this;
+    }
+
+    public function getPrenomUtilisateur(): ?string
+    {
+        return $this->prenom_utilisateur;
+    }
+
+    public function setPrenomUtilisateur(string $prenom_utilisateur): self
+    {
+        $this->prenom_utilisateur = $prenom_utilisateur;
+
+        return $this;
+    }
+}
